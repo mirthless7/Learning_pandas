0228298b0cc3e58362a2d1718ed3b132bd52fd23
Bryan <bryanleyva2005@gmail.com>
2023-10-10 21:06:02 -0400

    notes continued

---
+++ panda_notes.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+#lists and dictionaries
+column=["Mariya","John","Dmytro"]
+
+#dictionary made
+titled_column={"name":column,
+               "height":[6.1,6.2,5.7],
+               "weight":[270,300,160]}
+
+#turns the list into a dataframe
+data =pd.DataFrame(titled_column)
+
+#iloc means a row, selecting values from a dataframe
+select_column=data["weight"][1]
+select_row=data.iloc[1]["weight"]
+
+#manipulate dataframe values
+bmi=[]
+
+#6:28 for tommorow
+
+
+
+print(select_row)
